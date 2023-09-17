f8acc61d1a39217267c463de50b2593da8707038
samir-ambaliya <140295816+samir-ambaliya@users.noreply.github.com>
2023-09-17 06:17:32 -0700

    2 files modified, 3 added

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { UserFormComponent } from './form-data/form-data.component';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UserFormComponent,MatChipsModule],
   template: `
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular
     </a>
+    <app-user-form></app-user-form>
   `,
 })
 export class App {
